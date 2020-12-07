c85f98fcbaff62ee0559d1bea6d2023c8456bfe2
suhitk1 <suhitk2112@gmail.com>
2020-12-07 16:17:23 +0530

    updated answer

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const bodyParser = require("body-parser");
-const marioModel = require('./models/marioChar');
-
+const mario = require('./models/marioChar');
+const marioModel = mongoose.model('Mario',mario);
 // Middlewares
 app.use(express.urlencoded());
 
@@ -12,7 +13,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // your code goes here
-
+app.get('/mario', function (request, response) {
+    marioModel.find({}, function (err, result) {
+        if (err) {
+            response.send(err);
+        } else {
+            response.send(result);
+        }
+    });
+});
 
 
 module.exports = app;
